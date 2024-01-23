052a717d1ac5845a0b33caadb1eb7d1c540edc5b
Isto Nikula <isto.nikula@iki.fi>
2024-01-23 10:13:42 +0200

    fix(tx): support null returns from tx wrapper

--- realworld-app/web/src/main/kotlin/io/realworld/Tx.kt
+++ realworld-app/web/src/main/kotlin/io/realworld/Tx.kt
@@ -12,7 +12,7 @@ fun <A> runReadTx(txManager: PlatformTransactionManager, fn: suspend () -> A): A
       isolationLevel = TransactionDefinition.ISOLATION_READ_COMMITTED
       propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
     }
-    .execute { runBlocking { fn() } }!!
+    .execBlocking(fn)
 }
 
 fun <A> runWriteTx(txManager: PlatformTransactionManager, fn: suspend () -> A): A {
@@ -21,5 +21,15 @@ fun <A> runWriteTx(txManager: PlatformTransactionManager, fn: suspend () -> A): 
       isolationLevel = TransactionDefinition.ISOLATION_READ_COMMITTED
       propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
     }
-    .execute { runBlocking { fn() } }!!
+    .execBlocking(fn)
+}
+
+private fun <A> TransactionTemplate.execBlocking(fn: suspend () -> A): A {
+  // TransactionTemplate#execute is annotated as @Nullable which is used by Kotlin to infer nullability.
+  // However, the return value of the given fn is the one that defines whether result is nullable or not.
+  //
+  // fn supports implicit nullability as it's defined to return A (<A> is implicitly <A : Any?>), leaving the final
+  // decision on to the calling code that provides the fn implementation and typing.
+  @Suppress("UNCHECKED_CAST")
+  return execute { runBlocking { fn() } } as A
 }
